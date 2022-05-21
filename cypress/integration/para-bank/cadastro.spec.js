8082a7943c618a5bf2a0eae78dda36166e86803a
Felipe Matheus <felipemsoad@gmail.com>
2022-05-20 22:41:33 -0300

    Adicionado comando personalizado para limpar DB e registrar usuário

--- cypress/integration/para-bank/cadastro.spec.js
+++ cypress/integration/para-bank/cadastro.spec.js
@@ -14,34 +14,12 @@ describe ('Casos para cadastro de usuário', ()=>{
     })
 
     
-    it('Realizar cadastro', ()=>{
-        const cadastro = require("../../fixtures/registro.json")
+    it('Realizar novo cadastro', ()=>{
+        cy.limparDB() //Comando personalizado para limpar o banco de dados e voltar para a página de registro
+        cy.registrarUsuario()
+    })
 
-        //Preenchimento do formulário de registro
-        cy.get('input[id="customer.firstName"]').type(cadastro["customer.firstName"])
-        cy.get('input[id="customer.lastName"]').type(cadastro["customer.lastName"])
-        cy.get('input[id="customer.address.street"]').type(cadastro["customer.address.state"])
-        cy.get('input[id="customer.address.city"]').type(cadastro["customer.address.city"])
-        cy.get('input[id="customer.address.state"]').type(cadastro["customer.address.state"])
-        cy.get('input[id="customer.address.zipCode"]').type(cadastro["customer.address.zipCode"])
-        cy.get('input[id="customer.phoneNumber"]').type(cadastro["customer.phoneNumber"])
-        cy.get('input[id="customer.ssn"]').type(cadastro["customer.ssn"])
-
-        //Preenchimento de Usuário e Senha
-        cy.get('input[id="customer.username"]').type(cadastro["customer.username"])
-        cy.get('input[id="customer.password"]').type(cadastro["customer.password"])
-        cy.get('input[id="repeatedPassword"]').type(cadastro["customer.password"])
-
-        //Botão de registro
-        cy.get('input[value="Register"]').click()
-
-        cy.request({
-            method: 'POST',
-            url: 'https://parabank.parasoft.com/parabank/register.htm;*'
-        }).then((resposta)=>{
-            expect(resposta.status).to.be.eq(200)
-            //expect().to.be.eq(`Welcome ${cadastro["customer.username"]}`)
-        })
+    it('Realizar Cadastro de usuário já existente', ()=>{
 
     })
 })
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,39 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.Commands.add('limparDB', ()=>{
+    cy.contains('a', 'Admin Page').click()
+    cy.contains('button', "Clean").click()
+    cy.visit('register.htm')
+})
+
+Cypress.Commands.add('registrarUsuario', ()=>{
+    const cadastro = require("../../cypress/fixtures/registro.json")
+
+    //Preenchimento do formulário de registro
+    cy.get('input[id="customer.firstName"]').type(cadastro["customer.firstName"])
+    cy.get('input[id="customer.lastName"]').type(cadastro["customer.lastName"])
+    cy.get('input[id="customer.address.street"]').type(cadastro["customer.address.state"])
+    cy.get('input[id="customer.address.city"]').type(cadastro["customer.address.city"])
+    cy.get('input[id="customer.address.state"]').type(cadastro["customer.address.state"])
+    cy.get('input[id="customer.address.zipCode"]').type(cadastro["customer.address.zipCode"])
+    cy.get('input[id="customer.phoneNumber"]').type(cadastro["customer.phoneNumber"])
+    cy.get('input[id="customer.ssn"]').type(cadastro["customer.ssn"])
+
+    //Preenchimento de Usuário e Senha
+    cy.get('input[id="customer.username"]').type(cadastro["customer.username"])
+    cy.get('input[id="customer.password"]').type(cadastro["customer.password"])
+    cy.get('input[id="repeatedPassword"]').type(cadastro["customer.password"])
+
+    //Botão de registro
+    cy.get('input[value="Register"]').click()
+
+    cy.request({
+        method: 'POST',
+        url: 'https://parabank.parasoft.com/parabank/register.htm;*'
+    }).then((resposta)=>{
+        expect(resposta.status).to.be.eq(200)
+        //expect().to.be.eq(`Welcome ${cadastro["customer.username"]}`)
+    })
+})
